8d1565d5184af58cebdaa3f7884f2a0bf12dc18f
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-08-18 18:06:47 -0500

    isRegisteredAsParallelCapable

--- java-api/src/java9/java/xyz/wagyourtail/jvmdg/j9/stub/java_base/J_L_ClassLoader.java
+++ java-api/src/java9/java/xyz/wagyourtail/jvmdg/j9/stub/java_base/J_L_ClassLoader.java
@@ -7,10 +7,14 @@ import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import xyz.wagyourtail.jvmdg.util.Utils;
 import xyz.wagyourtail.jvmdg.version.Modify;
 import xyz.wagyourtail.jvmdg.version.Ref;
 import xyz.wagyourtail.jvmdg.version.Stub;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -65,6 +69,23 @@ public class J_L_ClassLoader {
     @Stub(ref = @Ref("Ljava/lang/ClassLoader;"))
     public static ClassLoader getPlatformClassLoader() {
         return ClassLoader.getSystemClassLoader();
+    }
+
+    private static final MethodHandles.Lookup IMPL_LOOKUP = Utils.getImplLookup();
+    private static final MethodHandle IS_REGISTERED;
+
+    static {
+        try {
+            Class<?> parallelLoaders = Class.forName("java.lang.ClassLoader$ParallelLoaders");
+            IS_REGISTERED = IMPL_LOOKUP.findStatic(parallelLoaders, "isRegistered", MethodType.methodType(boolean.class, Class.class));
+        } catch (NoSuchMethodException | IllegalAccessException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Stub
+    public final boolean isRegisteredAsParallelCapable(ClassLoader loader) throws Throwable {
+        return (boolean) IS_REGISTERED.invokeExact(loader.getClass());
     }
 
 }
