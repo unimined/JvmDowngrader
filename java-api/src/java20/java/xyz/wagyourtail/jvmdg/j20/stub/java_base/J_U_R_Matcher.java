f75131315432dc40094d3799e62415b1fff01f60
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-03-09 22:26:00 -0500

    static final

--- java-api/src/java20/java/xyz/wagyourtail/jvmdg/j20/stub/java_base/J_U_R_Matcher.java
+++ java-api/src/java20/java/xyz/wagyourtail/jvmdg/j20/stub/java_base/J_U_R_Matcher.java
@@ -3,12 +3,25 @@ package xyz.wagyourtail.jvmdg.j20.stub.java_base;
 import xyz.wagyourtail.jvmdg.util.Utils;
 import xyz.wagyourtail.jvmdg.version.Stub;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class J_U_R_Matcher {
+
+    private static final MethodHandles.Lookup IMPL_LOOKUP = Utils.getImplLookup();
+    private static final MethodHandle namedGroups;
+
+    static {
+        try {
+            namedGroups = IMPL_LOOKUP.findVirtual(Pattern.class, "namedGroups", MethodType.methodType(Map.class));
+        } catch (Throwable e) {
+            throw new AssertionError(e);
+        }
+    }
 
     @Stub
     public static boolean hasMatch(Matcher matchResult) {
@@ -22,8 +35,7 @@ public class J_U_R_Matcher {
 
     @Stub
     public static Map<String, Integer> namedGroups(Matcher matchResult) throws Throwable {
-        var impl_lookup = Utils.getImplLookup();
-        return (Map<String, Integer>) impl_lookup.findVirtual(Pattern.class, "namedGroups", MethodType.methodType(Map.class)).invokeExact(matchResult.pattern());
+        return (Map<String, Integer>) namedGroups.invokeExact(matchResult.pattern());
     }
 
 }
